3d849b46bdae650eb909ab8b47148cd5c8945e05
Dan McGrath <daniel.mcgrath9@gmail.com>
2018-02-16 11:40:00 -0500

    fix height of content wrapper

--- src/layouts/contentwrapper.js
+++ src/layouts/contentwrapper.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 import styled from 'styled-components'
 
 const ContentWrapper = styled.div`
-  height: 100vh;
+  height: 100%;
   width: 100%;
   @media(min-width: 1024px) {
     width: 80%;
