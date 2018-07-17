44afef16485389b854182aa0a0805107311053a7
Dan McGrath <daniel.mcgrath9@gmail.com>
2018-07-16 21:15:56 -0400

    remove attribition since i bought the icons

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,12 +3,6 @@ import styled from 'styled-components'
 
 import ContentWrapper from '../layouts/contentwrapper'
 
-const Footer = styled.footer`
-  padding-top: 85%;
-  font-size: 12px;
-  color: rgba(0, 0, 0, 0.6);
-  `
-
 const IconLink = styled.a`
   > svg {
     height: 40px;
@@ -50,7 +44,6 @@ const AboutPage = () => (
     <svg aria-labelledby="simpleicons-strava-icon" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title id="simpleicons-strava-icon">Strava icon</title><path d="M15.387 17.944l-2.089-4.116h-3.065L15.387 24l5.15-10.172h-3.066m-7.008-5.599l2.836 5.598h4.172L10.463 0l-7 13.828h4.169"/></svg>
     </IconLink>
 
-    <Footer>Thanks to Luke Salmon for the bike crank graphic from the Noun Project!</Footer>
   </ContentWrapper>
 )
 
