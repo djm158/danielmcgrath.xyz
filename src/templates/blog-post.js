57fb405f10795ebc0921ad2eee9c3eae5f6c26d5
Dan McGrath <daniel.mcgrath9@gmail.com>
2019-02-07 21:04:16 -0500

    add back html to blog post query

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -14,6 +14,7 @@ export default ({ data }) => {
 export const query = graphql`
   query BlogPostQuery($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
       frontmatter {
         title
       }
